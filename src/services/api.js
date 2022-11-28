507cc275c3bef97c6ebd7dd145faeb44ea312d04
Lucas Medeiros <103211800+LucasMedeiros7@users.noreply.github.com>
2022-11-28 17:18:16 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://resilia-challenge-api.herokuapp.com'
+  baseURL: 'https://resilia-challenge-api.onrender.com'
 });
 
 export default api;
